d03568f68bad88375d909234ac763c46200cd60c
MartynFewtrell <60819899+MartynFewtrell@users.noreply.github.com>
2022-03-13 20:28:13 +0000

    Added Pulumi Deployment Project, Resources Class library and base pipeline (#2)

---
+++ TWPC.ClubWebsite.Resources/Managers/ResourceManager.cs
@@ -0,0 +1,20 @@
+﻿using TWPC.ClubWebsite.Resources.Services;
+
+namespace TWPC.ClubWebsite.Resources.Managers
+{
+    public class ResourceManager
+    {
+        private IResourceGroupService _resourceGroupService;
+
+
+        public ResourceManager(IResourceGroupService resourceGroupService)
+        {
+            _resourceGroupService = resourceGroupService;
+        }
+
+        public void CreateResources()
+        {
+            var resourceGroup = _resourceGroupService.NewResourceGroup("TWPC-ClubWebsite", "UK South");
+        }
+    }
+}
